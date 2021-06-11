296f334e04e1ebe6b2693fa89f80d8e27159a9d5
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-06-11 17:06:11 +0530

    Added Signup Component
    Added Sign Up component and fucntions

--- src/Page/sign-in-and-sign-up/signin-signup.jsx
+++ src/Page/sign-in-and-sign-up/signin-signup.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import SignIn from "../../Components/SignIn/Sign-in.jsx"
+import SignUp from "../../Components/SignIn/signUp/sign-up.jsx";
 
-
+import "./sign-in-and-sign-up.styles.scss"
 const signInSignupPage = () => {
     return ( 
-        <div>
+        <div className="sign-in-and-sign-up">
             <SignIn />
+            <SignUp />
         </div>
      );
 }
