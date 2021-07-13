ccabb5d22195447dc8154c0a21645be9443d0bf7
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-07-13 15:35:26 +0530

    Added Cart to Users
    Added Cart Items to user

--- client/src/react/cart/cart.actions.js
+++ client/src/react/cart/cart.actions.js
@@ -1,24 +1,33 @@
-import cartTypes from './cart.types';
+import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-  type: cartTypes.TOGGLE_CART_HIDDEN
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
 export const addItem = item => ({
-  type: cartTypes.ADD_ITEM,
-  payload: item
-});
-
-export const clearItemFromCart = item => ({
-  type: cartTypes.CLEAR_ITEM_FROM_CART,
+  type: CartActionTypes.ADD_ITEM,
   payload: item
 });
 
 export const removeItem = item => ({
-  type: cartTypes.REMOVE_ITEM,
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+});
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
   payload: item
 });
 
 export const clearCart = () => ({
-  type: cartTypes.CLEAR_CART
+  type: CartActionTypes.CLEAR_CART
+});
+
+export const updateCartInFirebase = () => ({
+  type: CartActionTypes.UPDATE_CART_IN_FIREBASE
+});
+
+export const setCartFromFirebase = cartItems => ({
+  type: CartActionTypes.SET_CART_FROM_FIREBASE,
+  payload: cartItems
 });
